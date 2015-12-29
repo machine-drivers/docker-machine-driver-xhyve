cacdc4d4b5895be432bcac8b594618b7f782a626
Koichi Shiraishi <zchee.io@gmail.com>
2015-12-30 06:02:19 +0900

    Fix test script
    - Usage: ./scripts/test.bash go test -v
    
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-export GOPATH=/Users/`users | awk 'NR == 1 {print $1}'`/go
-export PATH=/usr/local/go/bin:$GOPATH/bin:$PATH
-export GOOS=darwin
-export GO15VENDOREXPERIMENT=1
-export GOARCH=amd64
-export GOMAXPROCS=8
-export MACHINE_DEBUG=1
-export MACHINE_DEBUG_DRIVER=1
-
-make "$1"
+sudo \
+  env GOROOT=$GOROOT \
+  env GOPATH="$GOPATH" \
+  env PATH=$PATH \
+  env GO15VENDOREXPERIMENT=1 \
+  env GOMAXPROCS=8 \
+  env MACHINE_DEBUG=1 \
+"$@"
