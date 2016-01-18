3bdafb21f3c7556cdc1097c532789f0d2fc52983
Koichi Shiraishi <k@zchee.io>
2016-01-18 16:35:19 +0900

    Add printf and required
    Signed-off-by: Koichi Shiraishi <k@zchee.io>

--- scripts/upgrade-docker.bash
+++ scripts/upgrade-docker.bash
@@ -1,7 +1,8 @@
 #/bin/sh
 set -ex
 
-# Upgrade docker scripts inside boot2docker
+# Upgrade docker scripts inside docker-machine vm
+# Required /etc/init.d/docker script inside vm
 # Usage:
 # $ cat ./scripts/upgrade-docker.bash | docker-machine ssh xhyve VERSION=1.10.0-rc1 sh
 
@@ -13,12 +14,15 @@ else
   DOCKER_DOWNLOAD_URL="https://test.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}"
 fi
 
-sudo sh -c "rm -f /usr/local/bin/docker && \
-  curl -fL -o /usr/local/bin/docker ${DOCKER_DOWNLOAD_URL} && \
-  chmod +x /usr/local/bin/docker && \
-  /etc/init.d/docker stop && \
-  /etc/init.d/docker start && \
-  sleep 3 && \
-  docker version"
+sudo sh -c "rm -f /usr/local/bin/docker \
+  && curl -fL -o /usr/local/bin/docker ${DOCKER_DOWNLOAD_URL} \
+  && chmod +x /usr/local/bin/docker \
+  && /etc/init.d/docker stop \
+  && /etc/init.d/docker start \
+  && printf '\033[0;36mWait for restart docker server...\033[0m\n\n' \
+  && sleep 3 \
+  && printf '\033[0;36mUpgraded.\033[0m\n' \
+  && printf '\033[0;36mdocker version\033[0m\n' \
+  && docker version"
 
 exit 0
