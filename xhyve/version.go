38ea03f7d9efcf929de5c035c3ad550332cd8aa6
Koichi Shiraishi <zchee.io@gmail.com>
2016-10-08 04:57:37 +0900

    xhyve: remove unnecessary variable and fix golint suggestions
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- xhyve/version.go
+++ xhyve/version.go
@@ -1,14 +1,9 @@
 package xhyve
 
 var (
-	// ConfigVersion dictates which version of the config.json format is
-	// used. It needs to be bumped if there is a breaking change, and
-	// therefore migration, introduced to the config file format.
-	ConfigVersion int = 1
-
 	// Version should be updated by hand at each release
-	Version string = "0.2.3"
+	Version = "0.2.3"
 
 	// GitCommit will be overwritten automatically by the build system
-	GitCommit string = "HEAD"
+	GitCommit = "HEAD"
 )
