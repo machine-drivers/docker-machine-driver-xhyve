f5d3697db7a36d65a2e550a376e6f9f90b3b0db3
Koichi Shiraishi <zchee.io@gmail.com>
2017-04-16 13:23:00 +0900

    xhyve: remove uuid comment
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- xhyve/uuid.go
+++ xhyve/uuid.go
@@ -7,16 +7,14 @@ package xhyve
 char uuid_str[37];
 
 extern inline char* uuidgen() {
-	// typedef unsigned char uuid_t;
 	uuid_t uuid;
 
-	// generate
+	// generate with random
 	uuid_generate_random(uuid);
 
 	// unparse (to string)
 	uuid_unparse_upper(uuid, uuid_str);
 
-	// return printf("%s\n", uuid_str);
 	return uuid_str;
 }
 */
