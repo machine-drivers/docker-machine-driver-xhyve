c290fd6efa3891782b023001e96cf8c8827d31e0
Koichi Shiraishi <zchee.io@gmail.com>
2017-04-20 20:32:53 +0900

    xhyve: bump version 0.3.2
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- xhyve/version.go
+++ xhyve/version.go
@@ -2,7 +2,7 @@ package xhyve
 
 var (
 	// Version should be updated by hand at each release
-	Version = "0.3.1"
+	Version = "0.3.2"
 
 	// GitCommit will be overwritten automatically by the build system
 	GitCommit = "HEAD"
